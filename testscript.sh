3482ca872fcb419548e6169d11fde9607d8c1104
bhanu.munagala <bhanuprakashmunagala@gmail.com>
2023-11-23 13:05:49 +0530

    done changes as per shell in whihc it is presenyt

--- testscript.sh
+++ testscript.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 mkdir testfolder
 echo created a testfolder
-
+#******************************#
 ls -lrta
+
+#This is teh script below
 cat testscript.sh
 rm -rf  testfolder
 echo Deleted a testfolder
+#******************************#
 ls -lrta
+
 # This is a comment line
 
