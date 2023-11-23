27c8bdc46736d160c25b83699f7bb8d2a0f0970a
bhanu.munagala <bhanuprakashmunagala@gmail.com>
2023-11-23 13:18:59 +0530

    done changes as per shell in which  it is present

--- testscript.sh
+++ testscript.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 mkdir testfolder
 echo created a testfolder
+echo showing you the list of files and folders ...
 ls -lrta
 echo now showing you the script ...
-echo @************************@
+echo _________________________________________________
 cat testscript.sh
-echo @*************************@
+echo _________________________________________________
 echo removing the folder now ...
 rm -rf  testfolder
 echo showing you the list of files and folders ...
