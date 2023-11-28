828afe246076522e36d9b78a7d388660d01b43bf
bhanu.munagala <bhanuprakashmunagala@gmail.com>
2023-11-28 12:56:34 +0530

    done changes as per shell in which  it is present

--- testscript.sh
+++ testscript.sh
@@ -21,7 +21,7 @@ echo -e "line1\nline2\nline3"
 echo -e "\e[31mText is in Red color \e[0mColor is cleared now"
 
 #command for variables
-var1 = date
-echo Todays date is $(var1)
+VR=date
+echo Todays date is ${VR}
 
 
